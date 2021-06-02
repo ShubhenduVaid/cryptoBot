7cefeb516ff419afd34d9bddd41191cf63f0933a
Shubhendu Vaid <vaid.shubhendu@gmail.com>
2021-06-02 22:35:30 +0100

    init

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { getBTC, getETH } from "./ticker";
 import { genericAction } from "./store";
 import { interval } from "./config";
 
-const startEngine = async () => {
+const startBot = async () => {
   const btc = await getBTC();
   const eth = await getETH();
   genericAction({ type: "btc", payload: btc });
@@ -10,8 +10,8 @@ const startEngine = async () => {
 };
 
 (async () => {
-  await startEngine();
+  await startBot();
   setInterval(async () => {
-    await startEngine();
+    await startBot();
   }, interval);
 })();
