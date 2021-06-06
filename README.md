8dc35e908608ee86ed77c4ef036989c2e257ac3c
Shubhendu Vaid <vaid.shubhendu@gmail.com>
2021-06-06 16:26:14 +0100

    refactor

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# cryptoBot
+# CryptoBot
 
-### Install NODE JS https://nodejs.org/en/download/
+## NodeJS Download
 
-#### run via terminal npm start
+> https://nodejs.org/en/download/
+
+## To Start Bot
+
+Open cmd/terminal and type
+
+> npm start
+
+## For Configuration
+
+### \* config.js
+
+### \* For now support GBP and BTC only
+
+## For Transaction Logs
+
+> leadger.txt
---
+++ utils.ts
@@ -0,0 +1,55 @@
+import fs from "fs";
+
+import store, { Actions, Selectors } from "./redux";
+
+const stream = fs.createWriteStream("./ledger.txt", { flags: "a" });
+
+const buyRequest = (diff: number) => {
+  const gbpCoeff = Selectors.getGBPPurchaseCoeff(store.getState());
+  const gbpBalance = Selectors.getGBPBalance(store.getState());
+  const coeffList = Object.keys(gbpCoeff).filter(
+    (coeff) => Number(coeff) < diff
+  );
+  const coeff = coeffList[coeffList.length - 1];
+  const buyAmount = gbpCoeff[coeff as string];
+  if (gbpBalance > buyAmount) {
+    store.dispatch(Actions.decrementGBP(buyAmount));
+    const lastBTCPricelist = Selectors.getBTCHistory(store.getState());
+    const lastBTCPrice = lastBTCPricelist[lastBTCPricelist.length - 1];
+    const btcToBuy = buyAmount / lastBTCPrice;
+    store.dispatch(Actions.incrementBTC(btcToBuy));
+    writeLogs(
+      `Bought BTC with GBP. GBP ${buyAmount} spent. BTC ${btcToBuy} bought.`
+    );
+  }
+};
+
+const sellRequest = (diff: number) => {
+  const btcCoeff = Selectors.getBTCPurchaseCoeff(store.getState());
+  const btcBalance = Selectors.getBTCBalance(store.getState());
+  const coeffList = Object.keys(btcCoeff).filter(
+    (coeff) => Number(coeff) < diff
+  );
+  const coeff = coeffList[coeffList.length - 1];
+  const sellAmount = btcCoeff[coeff as string];
+  const lastBTCPricelist = Selectors.getBTCHistory(store.getState());
+  const lastBTCPrice = lastBTCPricelist[lastBTCPricelist.length - 1];
+  const btcToSell = sellAmount / lastBTCPrice;
+  if (btcBalance > btcToSell) {
+    store.dispatch(Actions.incrementtGBP(sellAmount));
+    store.dispatch(Actions.decrementtBTC(btcToSell));
+    writeLogs(
+      `Sold BTC for GBP. BTC ${btcToSell} sold. GBP ${sellAmount} gained.`
+    );
+  }
+};
+
+const writeLogs = (data: string) => {
+  try {
+    stream.write(data + "\n");
+  } catch (error) {
+    console.error("Write to file failed with error :: ", error);
+  }
+};
+
+export { buyRequest, sellRequest };
